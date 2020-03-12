7e2c23f73128626f7a8882594e273ca91b01af9c
alex <acdespotakis@gmail.com>
2020-03-11 21:01:03 -0400

    Removed unused endpoint in green/

--- green/cash_flow/cscf/urls.py
+++ green/cash_flow/cscf/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('add/', views.add_company, name='add_companies'),
     path('detailed/', views.detailed_company_view, name='detailed_company_view'),
     path('list/', views.list_all_companies, name='list_all_companies'),
-    path('update/', views.update_company_data, name='update_companies')
 ]
